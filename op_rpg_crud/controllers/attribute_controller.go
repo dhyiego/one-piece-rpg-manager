efb2ccb375c7ef3e727c14b8a4ca490d7ba32e30
Diego <diegocampos2099@gmail.com>
2025-01-16 22:15:25 -0300

    Creating controllers

---
+++ op_rpg_crud/controllers/attribute_controller.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"rpg_manager/config"
+	"rpg_manager/models"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateAttribute(w http.ResponseWriter, r *http.Request) {
+	var attribute models.Attribute
+	json.NewDecoder(r.Body).Decode(&attribute)
+
+	// Calculate Charisma and Defense
+	attribute.Charisma = (attribute.Intelligence + attribute.Willpower) / 2
+	attribute.Defense = attribute.Vitality * 2
+
+	if result := config.DB.Create(&attribute); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(attribute)
+}
+
+func GetAllAttributes(w http.ResponseWriter, r *http.Request) {
+	var attributes []models.Attribute
+	config.DB.Find(&attributes)
+	json.NewEncoder(w).Encode(attributes)
+}
+
+func GetAttributeByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var attribute models.Attribute
+	if result := config.DB.First(&attribute, id); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(attribute)
+}
---
+++ op_rpg_crud/controllers/fruit_controller.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"op_rpg_crud/op_rpg_crud/models"
+	"rpg_manager/config"
+	"rpg_manager/models"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateFruit(w http.ResponseWriter, r *http.Request) {
+	var fruit models.Fruit
+	json.NewDecoder(r.Body).Decode(&fruit)
+
+	if result := config.DB.Create(&fruit); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(fruit)
+}
+
+func GetAllFruits(w http.ResponseWriter, r *http.Request) {
+	var fruits []models.Fruit
+	config.DB.Find(&fruits)
+	json.NewEncoder(w).Encode(fruits)
+}
+
+func GetFruitByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var fruit models.Fruit
+	if result := config.DB.First(&fruit, id); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(fruit)
+}
